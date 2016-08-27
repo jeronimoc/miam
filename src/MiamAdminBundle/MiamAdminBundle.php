4ddf275f11b2b0e1aee4611323401d3ae35439e4
jeronimo <jeronimocoello@gmail.com>
2016-08-27 14:28:53 -0300

    Add admin and data bundle

---
+++ src/MiamAdminBundle/MiamAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MiamAdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MiamAdminBundle extends Bundle
+{
+}
---
+++ src/MiamDataBundle/MiamDataBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MiamDataBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MiamDataBundle extends Bundle
+{
+}
